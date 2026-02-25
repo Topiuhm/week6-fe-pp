887bf3fa19f17237a2ccab3857bac97ccd32ae29
Markus Kannisto <m.kannisto01@gmail.com>
2026-02-25 13:52:57 +0200

    feat(list-books): fetch and display all books on HomePage

--- week6-fepp/frontend/src/components/BookListings.jsx
+++ week6-fepp/frontend/src/components/BookListings.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import BookListing from "./BookListing";
 
 const BookListings = () => {
+  const [books, setBooks] = useState([]);
+
+  async function fetchAllBooks() {
+    const response = await fetch("/api/books");
+    if (response.ok) {
+      const booksList = await response.json();
+      setBooks(booksList);
+    } else {
+      console.error("Failed to fetch books");
+    }
+  }
+
+  useEffect(() => {
+    fetchAllBooks();
+  }, []);
+
   return (
     <div className="book-list">
-      <BookListing />
+      {books && books.map((book, index) =>
+        <BookListing key={index} book={book} />
+      )}
     </div>
   );
 };
