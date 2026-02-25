18f7ec3b1eeb29c9a33685b6baf22126e4765255
Topiuhm <makinen.eeli03@gmail.com>
2026-02-25 14:25:38 +0200

    feat(book-page): add route and fetch single book by id

---
+++ week6-fepp/frontend/src/components/BookPreview.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
+export default function BookPreview({ book }) {
+    const navigate = useNavigate();
+    return (
+        <>
+            <h2>{book.title}</h2>
+            <p>Author: {book.author}</p>
+            <p>ISBN: {book.isbn}</p>
+            <p>Publisher: {book.publisher}</p>
+            <p>Genre: {book.genre}</p>
+            <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
+            <p>Due Date: {book.availability.dueDate || "N/A"}</p>
+            <p>Borrower: {book.availability.borrower || "None"}</p>
+            <button onClick={() => navigate("/")}>Back</button>
+        </>
+    );
+}
--- week6-fepp/frontend/src/pages/BookPage.jsx
+++ week6-fepp/frontend/src/pages/BookPage.jsx
@@ -1,16 +1,26 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import BookPreview from "../components/BookPreview";
+
 const BookPage = () => {
+  const [book, setBook] = useState(null);
+  const params = useParams();
+  const bookId = params.id;
+  async function fetchBook() {
+    const response = await fetch(`/api/books/${bookId}`);
+    if (response.ok) {
+      const bookData = await response.json();
+      setBook(bookData);
+    } else {
+      console.error("Failed to fetch book details");
+    }
+  }
+  useEffect(() => {
+    fetchBook();
+  }, []);
   return (
     <div className="book-preview">
-      <>
-        <h2></h2>
-        <p>Author: </p>
-        <p>ISBN: </p>
-        <p>Publisher: </p>
-        <p>Genre: </p>
-        <p>Available: </p>
-        <p>Due Date: </p>
-        <p>Borrower: </p>
-      </>
+      {book && <BookPreview book={book} />}
     </div>
   );
 };
